8c6b04cb71ffce279a8b8fcfc472f90fc7cc89fe
MicaelaFLipman <89950269+MicaelaFLipman@users.noreply.github.com>
2021-09-21 10:26:37 -0400

    Update case_study_01.R
    commit

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,39 @@
+###Case Study 1 
+###Group 4
+
+data ("iris")
+mean(iris$Petal.Length)
+petal_length_mean=mean(iris$Petal.Length)
+hist(iris$Petal.Length)
+
+
+###other way using ggplot
+ggplot(iris,aes(x=Petal.Length, fill=Species)) + 
+  geom_histogram(binwidth=.5, position="stack")
+
+
+
+###use esquisse package for better job at finding code. 
+esquisse::esquisser()
+
+library(ggplot2)
+
+ggplot(iris) +
+  aes(x = Petal.Length, fill = Species) +
+  geom_histogram(bins = 30L) +
+  scale_fill_manual(values = c(setosa = "#6DF873", 
+                               versicolor = "#0025C1", virginica = "#FF61C3")) +
+  labs(x = "Petal Length", y = "Count", fill = "Plant Species") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+ggplot(iris) +
+  aes(x = Petal.Length, fill = Species) +
+  geom_histogram(bins = 15L) +
+  scale_fill_manual(values = c(setosa = "#6DF873", 
+                               versicolor = "#0025C1", virginica = "#FF61C3")) +
+  labs(x = "Petal Length", y = "Count", fill = "Plant Species", title = "Petal Length of Irises") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
 data(iris)
 

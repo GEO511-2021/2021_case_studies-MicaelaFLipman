59c600d00e0084d735a8732936dac0b0f8467cbc
MicaelaFLipman <89950269+MicaelaFLipman@users.noreply.github.com>
2021-10-16 20:54:55 -0400

    commit case study 6
    commit case study 6

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,12 +1,27 @@
 library (ggplot2)
-library (gapminder) 
-gapminder
 install.packages("gapminder")
 library (gapminder)
 library (dplyr)
-data (gapminder)
-View (gapminder)
-nokuwait <- filter(gapminder, country != "Kuwait")
+str(gapminder)
+##filter kuwait
+gapminder %>%
+  filter(country != "Kuwait") 
+##plot number 1 
+ggplot(gapminder) + 
+  geom_point(aes(x=lifeExp, y = gdpPercap, color = continent, size=pop/10000)) + 
+  facet_wrap(~ year, nrow=1) + 
+  scale_y_continuous(trans = "sqrt") + 
+  theme_bw() + 
+  labs(x= "Life Expectancy", y = "GDP per capita", size = "Population (100k)")
 
+
+##prepare data for second plot 
+str(gapminder)
+gapminder %>%
+  group_by (continent, year) %>%
+gapminder_continent <-summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop), pop = sum(as.numeric(pop)))
+
+##creating the second row of plots 
+ggplot(gapminder,gapminder_continent) + geom_line() + geom_point() + geom_line(data=newdata,gapminder_continent) + geom_point(data=newdata,gapminder_continent) + facet_wrap() + theme_bw() + labs(...)
 
         
---
+++ week_06/Untitled.R
@@ -0,0 +1,51 @@
+###Case Study 6
+###Micaela Lipman 
+###October 12, 2021
+
+##Load packages 
+#install.packages("raster")
+#install.packages("sf")
+#install.packages("sp")
+#install.packages("spData")
+#install.packages("tidyverse")
+#install.packages ("ncdf4")
+library(sp)
+library(spData)
+library(tidyverse)
+library(sf)
+library(raster)
+library (ncdf4)
+
+###Uploading data 
+library(ncdf4)
+download.file("https://crudata.uea.ac.uk/cru/data/temperature/absolute.nc","crudata.nc")
+tmean=raster("crudata.nc")
+
+###Filter out antarctica
+data (world)
+str(world) 
+View(world)
+countries <-filter (world, continent != "Antarctica") 
+
+###Convert the world object to sp format
+  countries_sp<-as(countries, "Spatial") 
+  tmax_monthly <- getData(name = "worldclim", var="tmax", res=10)
+  tmax_monthly
+  plot(tmax_monthly)
+
+  ###convert to degrees celcuis by dividing by 10 
+gain(tmax_monthly)=.1
+  
+
+###find annual max in each country
+plot(tmax_monthly)
+tmax_annual <-max(tmax_monthly)
+names(tmax_annual) <- "tmax"
+polygon_max_country<-raster::extract(tmax_annual, countries_sp, fun=max, na.rm=TRUE, small=TRUE, sp=TRUE) %>%
+st_as_sf(polygon_max_country)
+
+###map the results
+ggplot()
+geom_sf()
++scale_fill_viridis_c(name="Annual\nMaximum\nTemperature (C)")
+
